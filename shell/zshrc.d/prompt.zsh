a65ea29ed100aea1810efe37ecc0af5961d4118f
Martin Oehlert <mo1987@gmx.net>
2026-04-21 07:52:46 +0200

    feat(shell): add set_poshcontext to export aictx current context

--- shell/zshrc.d/prompt.zsh
+++ shell/zshrc.d/prompt.zsh
@@ -1,1 +1,5 @@
 _cache_source oh-my-posh oh-my-posh init zsh --config ~/.poshthemes/craver_custom.omp.json --print
+
+function set_poshcontext() {
+  export AICTX_CURRENT=$(aictx current 2>/dev/null)
+}
