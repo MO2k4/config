ed5d4196d85bb705657f8da9d9ed56f82d4b2793
Martin Oehlert <mo1987@gmx.net>
2026-04-21 07:58:13 +0200

    fix(prompt): use correct nf-cod-project icon, remove unused set_poshcontext

--- shell/zshrc.d/prompt.zsh
+++ shell/zshrc.d/prompt.zsh
@@ -1,5 +1,1 @@
 _cache_source oh-my-posh oh-my-posh init zsh --config ~/.poshthemes/craver_custom.omp.json --print
-
-function set_poshcontext() {
-  export AICTX_CURRENT=$(aictx current 2>/dev/null)
-}
